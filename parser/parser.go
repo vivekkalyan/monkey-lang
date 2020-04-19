6e280202f4c6f8c18e1aeb3dcf09e0382a5a9435
Vivek Kalyan <hello@vivekkalyan.com>
2020-04-19 19:46:53 +0800

    Create skeleton for ast and parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"monkey-lang/ast"
+	"monkey-lang/lexer"
+	"monkey-lang/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// read two tokens, so that curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
