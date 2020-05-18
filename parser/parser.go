b34823678c33fbccb4c7ea32f94c04271bd3d7fa
Vivek Kalyan <hello@vivekkalyan.com>
2020-05-18 23:14:11 +0800

    Add boolean parsing

--- parser/parser.go
+++ parser/parser.go
@@ -71,6 +71,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	// create map of <token, fn> for infix fns and populate with respective
 	// token, fn pairs
@@ -342,4 +344,10 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	expression.Right = p.parseExpression(precedence)
 
 	return expression
+}
+
+// parse boolean
+func (p *Parser) parseBoolean() ast.Expression {
+	// return true or false for value based on the token that we see
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
